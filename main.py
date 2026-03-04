4ccbefabe62636f1f31557d3170cfd5cf40c6953
IanRodriguez <mirodriguez.dev@gmail.com>
2026-03-04 15:09:24 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import turtle
+
+import pandas
+import turtle
+from turtle import Turtle,Screen
+
+img = "blank_states_img.gif"
+
+screen = Screen()
+screen.bgcolor("black")
+screen.title("US State Game")
+
+screen.addshape(img)
+turtle.shape(img)
+
+data = pandas.read_csv("50_states.csv")
+state_list = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < len(state_list):
+    ans = screen.textinput("US State Game","Enter a state",  ).title()
+    if ans in guessed_states:
+        pass
+
+    elif ans in state_list:
+        guessed_states.append(ans)
+        t = Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == ans]
+        t.goto(state_data.x.item(),state_data.y.item())
+        t.write(ans)
+
+turtle.mainloop()
